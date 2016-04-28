8b93a226ddc553465371a2f6a7806982ad09ad2d
iain plimmer <iainplimmer@iains-iMac.local>
2016-04-28 11:05:28 +0100

    We've now got a free hand drawing mode, and in under an hour FabricJS is pretty impressive!

--- script/angudraw.js
+++ script/angudraw.js
@@ -5,57 +5,77 @@
             
             var self = this;
 
-            //  Publically available functions
-            this.DrawObjectOnCanvas = _drawObjectOnCanvas;
+            //  Publically available functions           
             this.AddNewRectangle = _addNewRectangle;
             this.AddNewCircle = _addNewCircle;
             this.SaveItems = _saveItems;
             this.ClearAllItems = _clearAllItems;
             this.ReloadSavedItems = _reloadSavedItems;
+            this.ToggleDrawingMode = _toggleFreeDrawing;
             
-            // Let's make a reference to our canvas to refer to in this controller
+            // Let's make a reference to our canvas to refer to in this controller and set some default values
             self.canvas = new fabric.Canvas('myCanvas'); 
-            self.ItemsOnCanvas = [];   
+            self.ItemsOnCanvas = [];            
+            self.canvas.freeDrawingBrush.color = 'red';
+            self.canvas.isDrawingMode = true;
             
             //  And also set some properties that we can use locally    
             self.DropX = 1;
             self.DropY = 1;
             self.Width = 500;
             self.Height = 500;
-            
+                    
             //  Manage the colours here
             self.CurrentSelectedColour = 'red';
-            self.SetColour = function(colour){
-                console.log('current selected colour:', colour)
-                self.CurrentSelectedColour = colour;
+            self.SetColour = function(val){
+                self.CurrentSelectedColour = val;
+                self.canvas.freeDrawingBrush.color = val;
             };
             
+            function _toggleFreeDrawing () {             
+                self.canvas.isDrawingMode = !self.canvas.isDrawingMode;
+            }
+            
             function _saveItems () {
-                var objs = this.canvas.getObjects();              
-                self.ItemsOnCanvas = JSON.parse(JSON.stringify(objs))        
+                var objs = self.canvas.getObjects();              
+                self.ItemsOnCanvas = JSON.parse(JSON.stringify(objs))   
+                console.log(self.ItemsOnCanvas);     
             }
             
             function _reloadSavedItems () {
-               this.canvas.clear();
+               self.canvas.clear();
+               self.canvas.isDrawingMode = true;
                self.ItemsOnCanvas.map(function(o) {
                     //  TO DO ! Each item we can redraw onto the canvas here :)
                     console.log(o);
-                    if (o.type == 'rect'){
+                    if (o.type == 'rect') {
                         _addNewRectangle(o.left, o.top, o.fill, o.width, o.height);
                     }
-                    else if (o.type == 'circle'){
+                    else if (o.type == 'circle') {
                         _addNewCircle(o.radius, o.fill, o.left, o.top);
                     }
-                    
+                    else if (o.type == 'path') {
+                        _addFreeDraw(o.path, o.stroke);
+                    }                  
                });
             }
             
             function _clearAllItems () {
-                this.canvas.clear();
+                self.canvas.clear();
             }
             
-            //  Mange adding to the canvas and the drop point, primitive but it works for this test!
-            function _drawObjectOnCanvas (drawnObject) {              
+            //  When reloading any free hand drawing to the cnavas, we use this
+            function _addFreeDraw(path, stroke) {
+                var path = new fabric.Path(path, {
+                    stroke: stroke, 
+                    fill: false
+                });
+                _drawObjectOnCanvas(path);    
+            }
+            
+            //  Manage adding to the canvas and the drop point, primitive but it works for this test!
+            function _drawObjectOnCanvas (drawnObject) { 
+                self.canvas.isDrawingMode = false;             
                 self.canvas.add(drawnObject);                 
                 if ((self.DropX < self.Width/2) || (self.DropY < self.Height/2)){
                     self.DropX +=10;
@@ -79,8 +99,7 @@
                     lockScalingY: true,
                     lockRotation: true
                 });
-                console.log(rect);
-                self.DrawObjectOnCanvas(rect);             
+                _drawObjectOnCanvas(rect);             
             };
             
             //  Function that allows a new circle to be drawn to the canvas
@@ -93,9 +112,8 @@
                     lockScalingX: true,
                     lockScalingY: true,
                     lockRotation: true
-                });
-                
-                self.DrawObjectOnCanvas(circle);
+                });                
+                _drawObjectOnCanvas(circle);
 
             };
             
